5f4b3fbcd5d96116e15dc2764dcdb8c5b804c1d7
Subha2707 <deysubahdip66@gmail.com>
2026-06-23 14:41:05 +0530

    Fix Google Callback error

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,8 @@ const User = require("../models/User");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.SERVER_URL || "http://localhost:5000"}/api/auth/google/callback`,
+  callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`,
+  
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
@@ -31,6 +32,10 @@ passport.use(new GoogleStrategy({
     return done(err, null);
   }
 }));
+console.log(
+  "Google Callback URL:",
+  `${process.env.SERVER_URL}/api/auth/google/callback`
+);
 
 passport.serializeUser((user, done) => done(null, user._id));
 passport.deserializeUser(async (id, done) => {
